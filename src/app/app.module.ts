12f6b41dd847d1a789610bd6779887bb63769a92
Cristhian Santos <cristhianfelipe0@gmail.com>
2021-11-02 13:19:35 -0300

    📦 refactor: refactoring of card text due to the translator and refactoring of styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContentLoaderModule } from '@ngneat/content-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardSearchAdviceComponent } from './components/card-search-advice/card-search-advice.component';
@@ -14,12 +17,15 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
   declarations: [AppComponent, ToolbarComponent, CardSearchAdviceComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule,
     MatCardModule,
     MatInputModule,
     MatButtonModule,
     HttpClientModule,
+    ContentLoaderModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
